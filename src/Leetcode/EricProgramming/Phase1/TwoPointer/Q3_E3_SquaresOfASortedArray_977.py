1cc4e33aac1c4f911c21c9445ad2efafe53b40bd
Anuja Srivastava <trykartehain@gmail.com>
2023-06-21 21:11:04 -0400

    21 june

--- src/Leetcode/EricProgramming/Phase1/TwoPointer/Q3_E3_SquaresOfASortedArray_977.py
+++ src/Leetcode/EricProgramming/Phase1/TwoPointer/Q3_E3_SquaresOfASortedArray_977.py
@@ -60,3 +60,68 @@ class Solution:
             p -= 1
 
         return output
+
+
+class Solution:
+
+    '''
+        * Question :
+            - Input array has both +ive and -ive numbers
+            - Input array is sorted
+            - return a sorted square array
+
+        * Solution :
+            - 2 pointers
+                - left : points at the 1st element
+                - right : points at the last element
+            - We have to leverage the fact that input array is sorted
+                - we know [left] is smaller than all the subsequent elements aka [right]
+                - Hence :
+                - if [left_sq] > [right_sq] :
+                    - [left_sq] is bigger than all the elements square till right
+                    - aka [left_sq] is the biggest and it should be present at the end of the output array
+                - if [right_sq] > [left_sq]:
+                    - [right_sq] is bigger than all the elements square till right
+
+        * Catch
+            - while condition should not be
+                - while left < right => This will skip the last element
+            - It should either be
+                - while left <= right
+                - while index >= 0
+
+        * Time Complexity :  O(n)
+        * Space Complexity : O(n)
+
+    '''
+
+class Solution:
+
+    def squaresOfASortedArray(self, nums: List[int]) -> List[int]:
+
+        ## initialization
+        left = 0
+        n = len(nums)
+        right = n - 1
+        output = [0] * n
+        index = n - 1
+
+        ## base condition
+        if n == 0:
+            return
+
+        ## code
+        while index >= 0:
+            left_sq = nums[left] * nums[left]
+            right_sq = nums[right] * nums[right]
+
+            if left_sq > right_sq:
+                left += 1
+                output[index] = left_sq
+            else:
+                right -= 1
+                output[index] = right_sq
+
+            index -= 1
+
+        return output
